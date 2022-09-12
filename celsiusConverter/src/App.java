b2f58eece2a86413da9e1bb70805ee861376f530
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-09-12 23:56:35 +0200

    read input and print converted degrees

--- celsiusConverter/src/App.java
+++ celsiusConverter/src/App.java
@@ -1,11 +1,19 @@
 package java_learn.celsiusConverter.src;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // celsius converter app
         double celsius = 0;
         double kelvin = celsius + 273;
-        double fahrenheit = 19 / 38 * celsius + 32;
-    }
+        double fahrenheit = 19/38 * celsius + 32;
 
-}
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Please Enter the temperature in celsius:");
+            celsius = scanner.nextDouble();
+        }
+        System.out.println("The temperature is " + celsius + "C in celsius");
+        System.out.println("The temperature is " + kelvin + "K in kelvin" );
+        System.out.println("The temperature is " + fahrenheit + "F in fahrenheits" );
+        }
+
+    }
