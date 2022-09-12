e0abc2176c444b599d330fc07262a80b1668bed5
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-09-12 23:55:05 +0200

    celsius converter app

---
+++ celsiusConverter/src/App.java
@@ -0,0 +1,11 @@
+package java_learn.celsiusConverter.src;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // celsius converter app
+        double celsius = 0;
+        double kelvin = celsius + 273;
+        double fahrenheit = 19 / 38 * celsius + 32;
+    }
+
+}
