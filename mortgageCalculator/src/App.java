d3bc940f1f6e0b3d255d3964c786dbe2a6983b4f
Patricknshimiyimana <lennoxmupi@gmail.com>
2022-09-18 23:12:27 +0200

    validate data using an infinite while loop

--- mortgageCalculator/src/App.java
+++ mortgageCalculator/src/App.java
@@ -1,4 +1,5 @@
 package mortgageCalculator.src;
+
 import java.text.NumberFormat;
 import java.util.Scanner;
 
@@ -7,24 +8,46 @@ public class App {
         final Byte MONTHS_IN_YEAR = 12;
         final Byte PERCENT = 100;
 
+        int principal = 0;
+        float montlyInterest = 0;
+        int numberOfPayments = 0;
+
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Principal: ");
-            int principal = scanner.nextInt();
+            while (true) {
+                System.out.print("Principal(1K - 100K): ");
+                principal = scanner.nextInt();
+                if (principal >= 1000 && principal <= 1_000_000)
+                    break;
+                System.out.println("Enter a value between 1K and 1M");
+            }
 
-            System.out.print("Annual Interest Rate: ");
-            float annualInterest = scanner.nextFloat();
-            float montlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+            while (true) {
+                System.out.print("Annual Interest Rate: ");
+                float annualInterest = scanner.nextFloat();
+                if (annualInterest >= 1 && annualInterest <= 30) {
+                    montlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+                    break;
+                }
+                System.out.println("Enter a value between 1 and 30");
 
-            System.out.print("Period (Years): ");
-            byte years = scanner.nextByte();
-            int numberOfPayments = years * MONTHS_IN_YEAR;
+            }
 
-            double mortgage = principal * 
-            (montlyInterest * Math.pow(1 + montlyInterest, numberOfPayments)) 
-            / Math.pow(1 + montlyInterest, numberOfPayments);
+            while (true) {
+                System.out.print("Period (Years): ");
+                byte years = scanner.nextByte();
+                if (years >= 1 && years <= 30) {
+                    numberOfPayments = years * MONTHS_IN_YEAR;
+                    break;
+                }
+                System.out.println("Enter a value between 1 and 30");
 
-            String mortgageFormatted = 
-            NumberFormat.getCurrencyInstance().format(mortgage);
+            }
+
+            double mortgage = principal *
+                    (montlyInterest * Math.pow(1 + montlyInterest, numberOfPayments))
+                    / Math.pow(1 + montlyInterest, numberOfPayments);
+
+            String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
 
             System.out.println();
             System.out.println("Mortgage: " + mortgageFormatted);
